be0edd1bda7d51e5397d39810969faa1768e8de2
250541003-dotcom <250541003@firat.edu.tr>
2025-12-03 14:02:05 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,10 +11,18 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+       // Bir değer döndüren metodu çağırıp sonucu kullanmasam bile program çalışmaya devam eder. 
+//Derleyici hata vermez ancak IDE “kullanılmayan değer” uyarısı gösterebilir.
+
+        getNumber();// burada return geliyor ama kullanılmıyor → program yine çalışır
+
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
-        // System.out.println(sayHello() + 7);
+        //Void metot değer döndürmediği için başka bir işlem içinde kullanılamaz.
+//System.out.println("boo!") + 7 ifadesi derleme hatası verir çünkü println() metodu void döner ve 
+//void bir sonuç matematiksel veya string işlemine katılamaz.
+
+        // System.out.println(sayHello() + 7);//hata verir.
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
